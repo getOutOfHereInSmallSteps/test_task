6c3215f7a397ccc9c55bbfac32ee7c152a0aa37d
Alex Bihas <alex.bigas.2004@gmail.com>
2024-03-08 13:46:52 +0200

    implemeted unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'test_task' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test_task');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test_task');
+  it('should display the current year in the footer', () => {
+    const footerElement: HTMLElement =
+      fixture.nativeElement.querySelector('footer');
+    const currentYear = new Date().getFullYear().toString();
+    expect(footerElement.textContent).toContain(currentYear);
   });
 });
--- src/app/shared/components/password-strength-checker/password-strength-checker.component.spec.ts
+++ src/app/shared/components/password-strength-checker/password-strength-checker.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { PasswordStrengthCheckerComponent } from './password-strength-checker.component';
+import { StrongPasswordPatternPipe } from '../../pipes/strong-password-pattern.pipe';
+import { MediumPasswordPatternPipe } from '../../pipes/medium-password-pattern.pipe';
+import { PASSWORD_COMPLEXITY } from '../../constants/enums';
 
 describe('PasswordStrengthCheckerComponent', () => {
   let component: PasswordStrengthCheckerComponent;
@@ -8,16 +12,71 @@ describe('PasswordStrengthCheckerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PasswordStrengthCheckerComponent]
-    })
-    .compileComponents();
-    
+      declarations: [
+        PasswordStrengthCheckerComponent,
+        StrongPasswordPatternPipe,
+        MediumPasswordPatternPipe,
+      ],
+      imports: [FormsModule, CommonModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(PasswordStrengthCheckerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize passwordStrength as empty', () => {
+    expect(component.passwordStrength).toEqual('');
+  });
+
+  it('should initialize showPassword as false', () => {
+    expect(component.showPassword).toEqual(false);
+  });
+
+  it('should toggle password visibility', () => {
+    component.showPassword = false;
+    component.togglePasswordVisibility();
+    expect(component.showPassword).toEqual(true);
+    component.togglePasswordVisibility();
+    expect(component.showPassword).toEqual(false);
+  });
+
+  it('should set password strength as EMPTY when password is empty', () => {
+    component.password = '';
+    component.checkStrength();
+    expect(component.passwordStrength).toEqual(PASSWORD_COMPLEXITY.EMPTY);
+  });
+
+  it('should set password strength as TOO_SHORT when password is less than the minimum length', () => {
+    component.password = 'abc';
+    component.checkStrength();
+    expect(component.passwordStrength).toEqual(PASSWORD_COMPLEXITY.TOO_SHORT);
+  });
+
+  it('should set password strength as STRONG when password is strong', () => {
+    const strongPassword = 'P@ssw0rd';
+    component.password = strongPassword;
+    component.checkStrength();
+    expect(component.passwordStrength).toEqual(PASSWORD_COMPLEXITY.STRONG);
+  });
+
+  it('should set password strength as MEDIUM when password is medium', () => {
+    const mediumPassword = 'Password123';
+    component.password = mediumPassword;
+    component.checkStrength();
+    expect(component.passwordStrength).toEqual(PASSWORD_COMPLEXITY.MEDIUM);
+  });
+
+  it('should set password strength as WEAK when password is weak', () => {
+    const weakPassword = 'password';
+    component.password = weakPassword;
+    component.checkStrength();
+    expect(component.passwordStrength).toEqual(PASSWORD_COMPLEXITY.WEAK);
   });
 });
